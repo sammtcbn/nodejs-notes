b9e22dc82195b61cab1ffc54ed03962d5c9cc214
sammtcbn <sammtcbn@gmail.com>
2021-01-29 15:04:31 +0800

    add script to install node.js v15

---
+++ install-nodejs-v15.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+if ! [ -x "$(command -v curl)" ]; then
+    echo 'Error: curl is not installed' >&2
+    exit 1
+fi
+
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+apt -y install nodejs
