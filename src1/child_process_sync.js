d3c68fe4a9fb7663767b020aab4c6192fe3e58be
sammtcbn <sammtcbn@gmail.com>
2021-03-17 15:23:17 +0800

    add trim for after execSync

--- src1/child_process_sync.js
+++ src1/child_process_sync.js
@@ -1,11 +1,11 @@
 process.env.LANG = 'en_US';
 
 var cmd1 = "dd if=/dev/zero of=foo.txt bs=5MB count=1";
-var result = require('child_process').execSync(cmd1).toString();
+var result = require('child_process').execSync(cmd1).toString().trim();
 console.log (result);
 
 var cmd2 = "ls -la foo.txt";
-var result = require('child_process').execSync(cmd2).toString();
+var result = require('child_process').execSync(cmd2).toString().trim();
 console.log (result);
 
 console.log ("done")
@@ -18,7 +18,6 @@ $ node child_process_sync.js
 5000000 bytes (5.0 MB, 4.8 MiB) copied, 0.0095522 s, 523 MB/s
 
 -rw-rw-r-- 1 adv adv 5000000 Apr  7 11:09 foo.txt
-
 done
 
 */
