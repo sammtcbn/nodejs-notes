4f2ab4112c3c0468f8e343b88c660c957e2bfbdd
sammtcbn <sammtcbn@gmail.com>
2023-08-24 16:59:11 +0800

    remove useless line

--- docker-nodejs-run.bash
+++ docker-nodejs-run.bash
@@ -1,4 +1,2 @@
 #!/bin/bash
-#docker run --rm -it --name golang -v $PWD/src:/go/src --workdir="/go/src" node:18
-#docker run --rm -it --name golang -v $PWD/src:/go/src --workdir="/go/src" node
 docker run --rm -it --name nodejs -v $PWD/src:/root/src --workdir="/root/src" node bash
